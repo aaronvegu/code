eb44fc8277f1cea0def7f9b1bf14b76376d9d0e0
Aaron Vegu <aaron@reckoner-rde.io>
2020-05-21 19:04:11 -0500

    2nd version of the bidirectional chat with sockets (Java) | Added JLabel with the state of connection of the service

--- java/sockets/codigo/GUI/ClienteBidireccionalGUI.java
+++ java/sockets/codigo/GUI/ClienteBidireccionalGUI.java
@@ -1,14 +1,13 @@
 import java.io.*;
-import java.net.*;
 import javax.swing.*;
-import java.awt.event.*;
 
-public class ClienteBidireccionalGUI // Clase Principal
+// == Clase Principal ==
+public class ClienteBidireccionalGUI 
 {
 
     public ClienteBidireccionalGUI() throws IOException
     {
-        EntornoGraficoCliente entorno = new EntornoGraficoCliente();
+        EntornoGraficoCliente entorno = new EntornoGraficoCliente("Cliente", 1234, 1235);
         entorno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
@@ -26,119 +25,16 @@ public class ClienteBidireccionalGUI // Clase Principal
 // == Interfaz Grafica ==
 class EntornoGraficoCliente extends JFrame 
 {
-    public EntornoGraficoCliente()
+    public EntornoGraficoCliente(String tipo, int puerto_salida, int puerto_entrada)
     {
 
         setBounds(1200, 300, 280, 350);
         
-        PanelChatCliente pc = new PanelChatCliente();
+        // Implementamos servicio
+        Servicio s = new Servicio(tipo, puerto_salida, puerto_entrada);
 
-        add(pc);
+        add(s);
 
         setVisible(true);
     }
-}
-
-class PanelChatCliente extends JPanel implements Runnable
-{
-    // Elementos para el socket
-    private final int PUERTO = 1234; // Puerto para la conexion
-    private final String HOST = "localhost";
-    private Socket clientSocket; // Socket del cliente
-    private ServerSocket serverSocket;
-    private DataOutputStream salidaAlServidor; // Datos de salida al servidor
-    private DataInputStream entradaDesdeServidor; // Datos de entrada desde el servidor
-
-    // String del mensaje que saldra al servidor
-    String salida = null;
-    // String del mensaje de entrada desde el servidor
-    String entrada = null;
-
-    // Objetos Graficos
-    JLabel titulo;
-    JButton boton;
-    JTextField campo;
-    JTextArea textArea;
-
-    public PanelChatCliente()
-    {
-        // Instanciamos objetos graficos
-        titulo = new JLabel("Cliente");
-        boton =  new JButton("Enviar mensaje");
-        campo = new JTextField(20);
-        textArea = new JTextArea(12, 20);
-
-        // Los agregamos a nuestro JPanel
-        add(titulo);
-        add(textArea);
-        add(campo);
-        add(boton);
-
-        /**
-         * == Para recibir mensajes ==
-         */
-        // Implementamos hilo
-        Thread hilo = new Thread(this);
-        // Iniciamos hilo
-        hilo.start();
-
-
-        /**
-         * == Para enviar mensajes ==
-         */
-        // Hacemos instancia del listener EnviarTexto
-        EnviarTexto et = new EnviarTexto();
-        // Hacemos que nuestro boton escuche el evento
-        boton.addActionListener(et);
-    }
-
-    @Override
-    public void run() {
-        try {
-            // Ponemos a la escucha a nuestro servidor mediante el socket en el puerto 1234
-            serverSocket = new ServerSocket(1235);
-
-            // Iteramos la comunicacion mientras se mantenga el entorno abierto:
-            while(true)
-            {
-                clientSocket = serverSocket.accept();
-                entradaDesdeServidor = new DataInputStream(clientSocket.getInputStream());
-                entrada = entradaDesdeServidor.readUTF();
-                textArea.append("Servidor: " + entrada + "\n");
-                clientSocket.close();
-            }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    // Listener
-    private class EnviarTexto implements ActionListener
-    {
-		@Override
-		public void actionPerformed(ActionEvent e) {            
-            try {
-                // Creamos nuestro socket
-                clientSocket =  new Socket(HOST, PUERTO);
-                // Creamos un flujo de salida del socket
-                salidaAlServidor = new DataOutputStream(clientSocket.getOutputStream());
-                // Guardamos el mensaje del cliente en la String salida
-                salida = campo.getText();
-                // Imprimos el mensaje enviado en nuestro chat
-                textArea.append("Cliente: " + salida + "\n");
-                // Enviamos el mensaje Salida como flujo de salida
-                salidaAlServidor.writeUTF(salida);
-                // Cerramos nuestro flujo de salida
-                salidaAlServidor.close();
-
-            } catch (IOException e1) {
-
-                System.out.println(e1.getMessage());
-
-            }
-			
-		}
-    }
-
 }
--- java/sockets/codigo/GUI/ServidorBidireccionalGUI.java
+++ java/sockets/codigo/GUI/ServidorBidireccionalGUI.java
@@ -1,15 +1,13 @@
 import java.io.*;
-import java.net.*;
 import javax.swing.*;
-import java.awt.event.*;
 
 public class ServidorBidireccionalGUI // Clase principal
 { 
 
     public ServidorBidireccionalGUI() throws IOException 
     {   
-        EntornoGraficoServidor gs = new EntornoGraficoServidor();
-        gs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        EntornoGraficoServidor gs = new EntornoGraficoServidor("Servidor", 1235, 1234);
+        gs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Para que se cierre nuestra interfaz cuando presionamos la x
     }
 
     public static void main(String[] args) throws IOException
@@ -26,127 +24,20 @@ public class ServidorBidireccionalGUI // Clase principal
 // == Interfaz Grafica ==
 class EntornoGraficoServidor extends JFrame
 {
+    
 
-    public EntornoGraficoServidor()
+    public EntornoGraficoServidor(String tipo, int puerto_salida, int puerto_entrada)
     {
 
-        setBounds(1200, 300, 280, 350);
+        setBounds(1200, 300, 280, 350); // Le ponemos tamanio al JFrame
         
-        PanelChatServidor pc = new PanelChatServidor();
+        // Implementamos servicio
+        Servicio s = new Servicio(tipo, puerto_salida, puerto_entrada);
 
-        add(pc);
+        add(s);
 
-        setVisible(true);
+        setVisible(true); // Lo hacemos visible
 
-    }
-
-}
-
-class PanelChatServidor extends JPanel implements Runnable
-{
-    // Objetos para sockets
-    private final int PUERTO = 1234; // Puerto para la conexion
-    private final String HOST = "localhost";    
-    private ServerSocket serverSocket; // Socket del servidor
-    private Socket clientSocket; // Socket del cliente
-    private DataInputStream entradaDesdeCliente; // Flujo de entrada de datos del cliente
-    private DataOutputStream salidaAlCliente; // Flujo de datos del servidor al cliente
-
-    // String del mensaje que saldra al servidor
-    String salida = null;
-    // String del mensaje de entrada desde el servidor
-    String entrada = null;
-
-    // Objetos Graficos
-    JLabel titulo;
-    JButton boton;
-    JTextField campo;
-    JPanel panelArea;
-    public JTextArea textArea;
-
-    public PanelChatServidor()
-    {
-        // Instanciamos objetos graficos
-        titulo = new JLabel("Servidor");
-        boton =  new JButton("Enviar mensaje");
-        campo = new JTextField(20);
-        textArea = new JTextArea(12, 20);
-
-        // Los agregamos a nuestro JPanel
-        add(titulo);
-        add(textArea);
-        add(campo);
-        add(boton);
-
-        // Implementamos hilo
-        Thread hilo = new Thread(this);
-        // Iniciamos hilo
-        hilo.start();
-
-        // Hacemos instancia del listener EnviarTexto
-        EnviarTexto et = new EnviarTexto();
-        // Hacemos que nuestro boton escuche el evento
-        boton.addActionListener(et);
-
-    }
-
-    @Override
-    public void run() { // Metodo run que esta a la escucha en todo momento
-        
-        try {
-            // Ponemos a la escucha a nuestro servidor mediante el socket en el puerto 1234
-            serverSocket = new ServerSocket(PUERTO);
-
-            // Iteramos la comunicacion mientras se mantenga el entorno abierto:
-            while(true)
-            {
-                // De encontrar un cliente, aceptar la conexion y crear el socket cliente
-                clientSocket = serverSocket.accept();
-                // Obtenemos la entrada proveniente desde el socket del cliente
-                entradaDesdeCliente = new DataInputStream(clientSocket.getInputStream());
-                // Hacemos lectura de ese flujo de datos (UTF) y los guardamos en la string de entrada
-                entrada = entradaDesdeCliente.readUTF();
-                // Concatenamos esos datos a nuestro TextArea
-                textArea.append("Cliente: " + entrada + "\n");
-                // Cerramos la conexion
-                clientSocket.close();
-            }
-            
-        } catch (Exception e1) {
-            System.out.println(e1.getMessage());
-
-        }
-
-    }
-
-    private class EnviarTexto implements ActionListener
-    {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            try {
-                // creamos nuestro socket
-                clientSocket = new Socket(HOST, 1235);
-                // Creamos un flujo de salida para enviar datos al socket
-                salidaAlCliente = new DataOutputStream(clientSocket.getOutputStream());
-                // Guardamos el mensaje del cliente en la String salida
-                salida = campo.getText();
-                // Imprimos el mensaje enviado en nuestro chat
-                textArea.append("Servidor: " + salida + "\n");
-                // Obtenemos flujo de salida del JTextField
-                salidaAlCliente.writeUTF(salida);
-                // Cerramos nuestro flujo de salida
-                salidaAlCliente.close();
-
-            } catch (Exception e1) {
-                
-                System.out.println(e1.getMessage());
-
-            }
-
-        }
-        
     }
 
 }
